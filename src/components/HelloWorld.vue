640a1992d79b9089ca37839a82b4b34cd1637fdc
germanmark <mgerman.software@gmail.com>
2022-04-21 21:02:40 -0600

    Added in class example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-alert v-if="isAlert" type="error">
+      Wrong username or password
+    </v-alert>
+    <v-alert v-if="isLoggedIn" type="success">
+      Login successful
+    </v-alert>
+    <v-text-field
+            v-model="username"
+            label="Username"
+            clearable
+          ></v-text-field>
+    <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min, rules.max]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+          <v-btn
+            elevation="2"
+            color="primary"
+            @click="login"
+          >Log in</v-btn>
+          <v-btn
+            elevation="2"
+            color="red lighten-2"
+            @click="logout"
+          >Log out</v-btn>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios'
+import cookies from 'vue-cookies'
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => {
+      return {
+        username : undefined,
+        show1: false,
+        password: 'Password',
+        isAlert : false,
+        isLoggedIn : false,
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 10 || 'Min 8 characters',
+          max: v => v.length <= 25 || 'Maximum 25 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+      }
+    },
+    methods: {
+      login() {
+        axios.request({
+          url: "https://reqres.in/api/login",
+          method: "POST",
+          headers : {
+            'Content-Type' : 'application/json'
+          },
+          data : {
+            "email": this.username,
+            "password": this.password
+          }
+        }).then((response)=>{
+          cookies.set('sessionToken', response.data.token);
+          this.isAlert = false;
+          this.isLoggedIn = true;
+          console.log(response);
+        }).catch(()=>{
+          this.isAlert = true;
+        })
+      },
+      logout(){
+        cookies.remove('sessionToken');
+      }
+    },
+  }
+</script>
